381bccaccbbe57510cd1ce075ab7c0092241008a
nikolastefanov <nikolay_stefanov1965@abv.bg>
2024-01-02 21:07:08 +0200

    Villian_Names

---
+++ ADO.NET/02_VillainNames/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _02_VillainNames
+{
+    public class StartUp
+    {
+        private const string ConnectionString =
+         @"Server=.;Database=MinionDB; Integrated Security=true";
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+
+            sqlConnection.Open();
+            using (sqlConnection)
+            {
+                string getCreateDatabaseText =
+                       @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount
+                                 FROM Villains AS v
+                                 JOIN MinionsVillains AS mv ON v.Id = mv.VillainId
+                                 GROUP BY v.Id, v.Name
+                                 HAVING COUNT(mv.VillainId) > 3
+                                  ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand sqlCommand = new SqlCommand(getCreateDatabaseText, sqlConnection);
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string name=reader["Name"]?.ToString();
+                    string minionsCount = reader["MinionsCount"]?.ToString();
+
+                    Console.WriteLine($"{name} - {minionsCount}");
+                }
+            }
+        }
+    }
+}
